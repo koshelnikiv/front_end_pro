4c386ed4e474cd305ae1e923d8aa4890e9a025a7
koshelnikiv <koshelnikiv@i.ua>
2025-03-09 11:09:00 +0200

    home-03-2

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function getStrings() {
+    let firstString = document.querySelector('.firstString').value;
+    let secondString = document.querySelector('.secondString').value;
+    let thirdString = document.querySelector('.thirdString').value;
+
+    console.log(`Ваш текст: 
+${firstString}
+${secondString}
+${thirdString}`);
+}
+
